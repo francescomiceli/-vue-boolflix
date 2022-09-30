5b06d6518c475869cbc5722bceb6d66bea9efc4e
francesco.miceli <francescomiceli@outlook.com>
2022-09-30 13:18:23 +0200

    End project

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <HeaderComponent @textToSearch='inputAssign'/>
+    <MainComponent :movies='ArrayMovies' :serieTv='ArraySerieTv'/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import MainComponent from './components/MainComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent
+},
+data(){
+  return{
+    resultInput : '',
+    ArrayMovies : [],
+    ArraySerieTv : [],
+  }
+},
+  created(){
+   
+  },
+  methods : {
+    inputAssign(insertText){
+      this.resultInput = insertText
+
+      axios
+        .get(`https://api.themoviedb.org/3/search/movie?api_key=654289286d1765757140382fe651ed97&language=it-IT&query=${this.resultInput}&page=1&include_adult=false`)
+        .then((response) => {
+          console.log('movie' , response)
+          console.log('movie',response.data.results)
+          this.ArrayMovies = response.data.results
+        })
+        axios
+          .get(`https://api.themoviedb.org/3/search/tv?api_key=654289286d1765757140382fe651ed97&language=it-IT&query=${this.resultInput}&include_adult=false`)
+          .then ((response) =>{
+            console.log('serie tv',response.data.results)
+            this.ArraySerieTv = response.data.results
+          })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,77 @@
+<template>
+  <header>
+    <div></div>
+    <div class="searchNavigation">
+      <h1>BoolFlix</h1>
+      
+      <ul>
+        <li><a href="">Film</a></li>
+        <li><a href="">Serie</a></li>
+        <li><a href="">La mia lista</a></li>
+      </ul>
+      <div class="searchbar">
+        <input type="text" placeholder="Cerca il tuo Film..." v-model="insertText" @keyup.enter="$emit('textToSearch', insertText)">
+        <button @click="$emit('textToSearch', insertText)">Cerca!</button>
+    </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+name : 'HeaderComponent',
+data(){
+    return{
+        insertText : '',
+    }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+
+  header{
+    background-color: black;
+  }
+.searchNavigation{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h1{
+    color: #E3170A;
+    padding: 15px;
+    font-size: 3rem;
+  };
+
+  ul{
+    color: white;
+    list-style-type: none;
+    display: flex;
+  }
+
+  ul li {
+    margin: 15px;
+  }
+  
+  a {
+    text-decoration: none;
+    color: white;
+  }
+
+  input{
+    border: none;
+    padding: 5px;
+    border-radius: 5px; 
+    margin-right: 10px;
+  };
+  button{
+    background-color: red;
+    border: none;
+    padding: 5px;
+    border-radius: 5px;
+  }
+}
+.searchbar{
+  padding: 0 15px;
+}
+</style>
